8e327736c18ce13d6eb883dab03feeb0e231b906
Antony Bartlett <antony.bartlett@ukri.org>
2022-03-19 19:05:44 +0000

    Attempt a build script

---
+++ build.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const resolve = promisify(require('resolve'));
+const cpy = require('cpy');
+
+const pkgDir = async (name) => {
+	let foundDir;
+
+	const isDirectory = (dir, cb) => {
+		fs.stat(dir, (err, stat) => {
+			if (!err) {
+				if (stat.isDirectory()) {
+					foundDir = dir;
+				}
+				return cb(null, stat.isDirectory());
+			}
+			if (err.code === 'ENOENT' || err.code === 'ENOTDIR') return cb(null, false);
+			return cb(err);
+		});
+	};
+
+	try {
+		await resolve(name, { isDirectory });
+	}
+	catch (e) {
+		if (!foundDir) {
+			throw e;
+		}
+	}
+	return foundDir;
+};
+
+const loggedCopy = (from, to) => {
+	// eslint-disable-next-line no-console
+	console.log(`Copying ${from} to ${to}`);
+	return cpy(from, to);
+};
+
+(async() => {
+	const wasmerDir = await pkgDir('@wasmer/wasm-terminal');
+	await loggedCopy(`${wasmerDir}/wasm-terminal/lib/xterm/**/*`, 'src/assets/wasm-terminal');
+	await loggedCopy(`${wasmerDir}/wasm-terminal/lib/workers/**/*`, `src/assets/wasm-terminal`);
+	await loggedCopy(`${wasmerDir}/wasm-transformer/lib/wasm-transformer.wasm`, 'src/assets/wasm-transformer');
+})();
+
